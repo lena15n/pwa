98be7cfb1f7aa0dcd6dcf078f052818981f359ec
Nazarova Elena <ln15ln@rambler.ru>
2017-06-19 23:50:25 +0400

    add web-anim-js

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -14,7 +14,6 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
   ],
   navigateFallback: 'index.html',
 };
